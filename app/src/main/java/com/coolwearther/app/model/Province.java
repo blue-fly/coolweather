f3c2de0e26b59ee9d1c5111f414077872c1b92a0
duan <1418929937@qq.com>
2015-08-29 17:39:32 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/com/coolwearther/app/model/Province.java
@@ -0,0 +1,34 @@
+package com.coolwearther.app.model;
+
+/**
+ * Created by dellpc on 2015-08-29.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
